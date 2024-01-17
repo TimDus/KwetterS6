bf3f7c2db909cf69dfad6b1a47d2e82abca54f3c
TimDus <timvddussen@gmail.com>
2024-01-18 00:04:02 +0100

    Update Test.js

--- Kubernetes/LoadTesting/Test.js
+++ Kubernetes/LoadTesting/Test.js
@@ -3,7 +3,7 @@ import http from 'k6/http';
 export default function () {
 
     const url = "http://kwetter.localhost:9080/api/Kweet/Create";
-    const payload = JSON.stringify({ "customerId": 1, "text": 'today is a nice day', });
+    const payload = JSON.stringify({ "customerId": 1, "text": 'today is a nice daya', });
     const params = {
         headers: {
             'Content-Type': 'application/json',
